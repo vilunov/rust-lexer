ddd3e36fafc5a3e0bdbbd2c51f98d9e7f83af409
Nikita Vilunov <nikitaoryol@gmail.com>
2018-09-22 17:42:08 +0300

    +! tests

--- src/test.rs
+++ src/test.rs
@@ -1,13 +1,35 @@
-use token::tokenize;
 use token::BinaryOperator::*;
 use token::Token::*;
+use token::*;
+
+struct TestCase(&'static str, &'static [Token]);
+
+const TESTS: &[TestCase] = &[
+    TestCase(
+        "255+1488",
+        &[LiteralInt, BinaryOperator(Plus), LiteralInt, Eof],
+    ),
+    TestCase(
+        "<>=<<=1",
+        &[
+            LessThan,
+            GreaterEqual,
+            BinaryOperatorAssignment(Shl),
+            LiteralInt,
+            Eof,
+        ],
+    ),
+    TestCase(
+        "\"This is a bucket!\"+\"Dear god\"",
+        &[LiteralStr, BinaryOperator(Plus), LiteralStr, Eof],
+    ),
+];
 
 #[test]
 fn t1() {
-    let input = "255+255";
-    let tokens = tokenize(input.chars());
-    assert_eq!(
-        tokens,
-        vec![LiteralInt, BinaryOperator(Plus), LiteralInt, Eof]
-    );
+    for TestCase(input, output) in TESTS {
+        let tokens = tokenize(input.chars());
+        assert_eq!(tokens, *output);
+        println!("Expression {} tokenized successfully!", input);
+    }
 }
